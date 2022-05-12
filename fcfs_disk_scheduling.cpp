50e3e477e747fdcd7c2d5ba358dc612a1b223519
Aditya <imadityasahni101@gmail.com>
2022-05-12 13:34:46 +0530

    again changes

--- fcfs_disk_scheduling.cpp
+++ fcfs_disk_scheduling.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 using namespace std;
+//  = {82,170,43,140,24,16,190}
+int main()
+{
+    int num_processes, current_position = 50, seek_time = 0;
+    cout << "Enter number of processes : ";
+    cin >> num_processes;
 
-int main(){
-    int current_position = 50, seek_time = 0, temp;
-    int position[] = {82,170,43,140,24,16,190};
-    int length_position = sizeof(position)/sizeof(position[0]);
-    for(int i = 0; i<length_position; i++){
-        temp = position[i] - current_position;
-        if(temp <0){
-            temp = (-temp);
+    int position[num_processes], indiv_seek_time[num_processes];
+    cout << "Enter current position : ";
+    cin >> current_position;
+
+    for (int i = 0; i < num_processes; i++)
+    {
+        cout << "Enter position of process " << i + 1 << " : ";
+        cin >> position[i];
+    }
+
+    for (int i = 0; i < num_processes; i++)
+    {
+        indiv_seek_time[i] = position[i] - current_position;
+        if (indiv_seek_time[i] < 0)
+        {
+            indiv_seek_time[i] = (-indiv_seek_time[i]);
         }
-        seek_time += temp;
+        seek_time += indiv_seek_time[i];
         current_position = position[i];
     }
-    cout<<"Seek time = "<<seek_time;
+    cout << "Seek time of individual processes are : ";
+    for (int i = 0; i < 7; i++)
+    {
+        cout << indiv_seek_time[i] << " ";
+    }
+    cout << "\nSeek time = " << seek_time;
     return 0;
 }
